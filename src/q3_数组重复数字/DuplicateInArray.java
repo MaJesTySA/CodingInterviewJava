1decd419080ba82eef4c8666c27b35471119f6b6
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-08-10 17:20:49 +0800

    Update and rename DuplicateNum.java to DuplicateInArray.java

--- src/q3_数组重复数字/DuplicateInArray.java
+++ src/q3_数组重复数字/DuplicateInArray.java
@@ -2,28 +2,28 @@ package q3_数组重复数字;
 
 import java.util.*;
 
-public class DuplicateNum {
+public class DuplicationInArray {
     public static void main(String[] args) {
-        int[] arr={  2, 0, 2, 1, 4 };
+        int[] arr = {2, 0, 2, 1, 4};
         System.out.println(getDuplicateWithHash(arr));
     }
 
     /*
         空间复杂度 HashMpa O(N)，时间复杂度O(N)。
      */
-    private static Set<Integer> getDuplicateWithHash(int[] arr){
-        Set<Integer> result=new HashSet<>();
-        if (arr==null || arr.length<0){
+    private static Set<Integer> getDuplicateWithHash(int[] arr) {
+        Set<Integer> result = new HashSet<>();
+        if (arr == null || arr.length < 0) {
             return result;
         }
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i]<0 || arr[i]>arr.length-1)
+            if (arr[i] < 0 || arr[i] > arr.length - 1)
                 return result;
         }
-        HashMap<Integer,Integer> numsMap=new HashMap();
+        HashMap<Integer, Integer> numsMap = new HashMap();
         for (int i = 0; i < arr.length; i++) {
-            if (numsMap.get(arr[i])==null){
-                numsMap.put(arr[i],arr[i]);
+            if (numsMap.get(arr[i]) == null) {
+                numsMap.put(arr[i], arr[i]);
             } else
                 result.add(arr[i]);
         }
@@ -33,23 +33,23 @@ public class DuplicateNum {
     /*
         空间复杂度O(1)，虽然有两重循环，但是时间复杂度是O(N)。
      */
-    private static Set<Integer> getDuplicateWithLessSpace(int[] arr){
-        Set<Integer> result=new HashSet<>();
-        if (arr==null || arr.length<0){
+    private static Set<Integer> getDuplicateWithLessSpace(int[] arr) {
+        Set<Integer> result = new HashSet<>();
+        if (arr == null || arr.length < 0) {
             return result;
         }
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i]<0 || arr[i]>arr.length-1)
+            if (arr[i] < 0 || arr[i] > arr.length - 1)
                 return result;
         }
         for (int i = 0; i < arr.length; i++) {
-            while (arr[i]!=i){
-                if (arr[i]==arr[arr[i]]){
+            while (arr[i] != i) {
+                if (arr[i] == arr[arr[i]]) {
                     result.add(arr[i]);
-                } else{
-                    int temp=arr[i];
-                    arr[i]=arr[temp];
-                    arr[temp]=temp;
+                } else {
+                    int temp = arr[i];
+                    arr[i] = arr[temp];
+                    arr[temp] = temp;
                 }
             }
         }
