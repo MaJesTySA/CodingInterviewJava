06a2aa5f7c4bd37e8f9f9596a72503823d9e7682
MaJesTySA <363644935@qq.com>
2019-08-24 11:38:30 +0800

    revise

--- src/q08_二叉树的下一个节点/GetNextNode.java
+++ src/q08_二叉树的下一个节点/GetNextNode.java
@@ -1,6 +1,5 @@
 package q08_二叉树的下一个节点;
 
-
 import utils.BinaryTreeNode;
 
 import java.util.ArrayList;
@@ -9,10 +8,44 @@ import java.util.List;
 public class GetNextNode {
     public static void main(String[] args) {
         test1();
-        System.out.println("======");
         test2();
-        System.out.println("======");
         test3();
+    }
+
+    private static BinaryTreeNode getNextNode(BinaryTreeNode node) {
+        if (node == null)
+            return null;
+        BinaryTreeNode nextNode = null;
+        //如果该节点右节点不为空，则找右节点的最左节点
+        if (node.right != null) {
+            BinaryTreeNode tempNode = node.right;
+            while (tempNode.left != null) {
+                tempNode = tempNode.left;
+            }
+            nextNode = tempNode;
+        }
+        //如果该节点右节点为空，则看该节点是否是父节点的左节点
+        else if (node.parent != null) {
+            BinaryTreeNode curNode = node;
+            BinaryTreeNode pareNode = node.parent;
+            while (pareNode != null && curNode == pareNode.right) {
+                curNode = pareNode;
+                pareNode = pareNode.parent;
+            }
+            nextNode = pareNode;
+        }
+        return nextNode;
+    }
+
+    private static void printNodes(List<BinaryTreeNode> lists) {
+        for (int i = 0; i < lists.size(); i++) {
+            try {
+                System.out.println("节点: " + lists.get(i).value + "的后继结点: " + getNextNode(lists.get(i)).value);
+            } catch (Exception e) {
+                System.out.println("节点: " + lists.get(i).value + "的后继结点: null");
+            }
+        }
+        System.out.println("=========");
     }
 
     private static void test1() {
@@ -24,13 +57,13 @@ public class GetNextNode {
         BinaryTreeNode node5 = new BinaryTreeNode(7);
         BinaryTreeNode node6 = new BinaryTreeNode(9);
         BinaryTreeNode node7 = new BinaryTreeNode(11);
-        node1.right = node2;
+        node1.left = node2;
         node1.right = node3;
         node2.parent = node1;
-        node2.right = node4;
+        node2.left = node4;
         node2.right = node5;
         node3.parent = node1;
-        node3.right = node6;
+        node3.left = node6;
         node3.right = node7;
         node4.parent = node2;
         node5.parent = node2;
@@ -73,16 +106,6 @@ public class GetNextNode {
         printNodes(lists);
     }
 
-    private static void printNodes(List<BinaryTreeNode> lists) {
-        for (int i = 0; i < lists.size(); i++) {
-            try {
-                System.out.println("节点: " + lists.get(i).value + "的后继结点: " + getNextNode(lists.get(i)).value);
-            } catch (Exception e) {
-                System.out.println("节点: " + lists.get(i).value + "的后继结点: null");
-            }
-        }
-    }
-
     private static void test3() {
         System.out.println("只有右节点");
         BinaryTreeNode node1 = new BinaryTreeNode(2);
@@ -104,31 +127,6 @@ public class GetNextNode {
         node1.inOrder();
         System.out.println();
         printNodes(lists);
-    }
-
-    private static BinaryTreeNode getNextNode(BinaryTreeNode node) {
-        if (node == null)
-            return null;
-        BinaryTreeNode nextNode = null;
-        //如果该节点右节点不为空，则找右节点的最左节点
-        if (node.right != null) {
-            BinaryTreeNode tempNode = node.right;
-            while (tempNode.right != null) {
-                tempNode = tempNode.right;
-            }
-            nextNode = tempNode;
-        }
-        //如果该节点右节点为空，则看该节点是否是父节点的左节点
-        else if (node.parent != null) {
-            BinaryTreeNode curNode = node;
-            BinaryTreeNode pareNode = node.parent;
-            while (pareNode != null && curNode == pareNode.right) {
-                curNode = pareNode;
-                pareNode = pareNode.parent;
-            }
-            nextNode = pareNode;
-        }
-        return nextNode;
     }
 }
 
