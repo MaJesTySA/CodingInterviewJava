b1794181b4bb622f39927164c81696bffdeb9029
MaJesTySA <363644935@qq.com>
2019-08-27 10:51:37 +0800

    Update Power.java

--- src/q16_数值的整数次方/Power.java
+++ src/q16_数值的整数次方/Power.java
@@ -13,6 +13,32 @@ public class Power {
 
     private static boolean invalidInput;
 
+    private static double powWithBit(double base, int exp) {
+        invalidInput = false;
+        if (base == 0.0 && exp < 0) {
+            invalidInput = true;
+            return 0.0;
+        }
+        if (exp < 0) {
+            double result = powerWithBit(base, -exp);
+            return 1.0 / result;
+        }
+        return powerWithBit(base, exp);
+    }
+
+    private static double powerWithBit(double base, int exp) {
+        if (exp == 0)
+            return 1;
+        if (exp == 1)
+            return base;
+        double result = powerWithBit(base, exp >> 1);
+        result *= result;
+        //如果exp是奇数
+        if ((exp & 0x1) == 1)
+            result *= base;
+        return result;
+    }
+
     private static double pow(double base, int exp) {
         invalidInput = false;
         if (base == 0.0 && exp < 0) {
@@ -27,23 +53,25 @@ public class Power {
     }
 
     private static double power(double base, int exp) {
-        if (exp == 0)
-            return 1;
-        if (exp == 1)
-            return base;
-        double result = power(base, exp >> 1);
-        result *= result;
-        //如果exp是奇数
-        if ((exp & 0x1) == 1)
+        double result = 1.0;
+        for (int i = 0; i < exp; i++) {
             result *= base;
+        }
         return result;
     }
 
     private static void test(String name, double base, int exp, double expectedRes, boolean expectedFlag) {
         System.out.println(name);
-        double res = pow(base, exp);
-        System.out.println("expected result = " + expectedRes + " result = " + res);
-        System.out.println("expected flag = " + expectedFlag + " flag = " + invalidInput);
-        System.out.println("===================================");
+        System.out.print("位运算求解: ");
+        if (powWithBit(base, exp) == expectedRes && invalidInput == expectedFlag)
+            System.out.println("Passed.");
+        else
+            System.out.println("Failed.");
+        System.out.print("循环法求解: ");
+        if (pow(base, exp) == expectedRes && invalidInput == expectedFlag)
+            System.out.println("Passed.");
+        else
+            System.out.println("Failed.");
+        System.out.println("===============");
     }
 }
