224bd127a13e36a8bfc12829914448fedd67746d
MaJesTySA <363644935@qq.com>
2019-08-27 10:01:57 +0800

    Update NumberOf1InBinary.java

--- src/q15_二进制1的个数/NumberOf1InBinary.java
+++ src/q15_二进制1的个数/NumberOf1InBinary.java
@@ -10,6 +10,40 @@ public class NumberOf1InBinary {
         test6();
     }
 
+    //输入负数会导致死循环
+    private static int numberOfOneRight(long n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) > 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    private static int numberOfOneLeft(long n) {
+        int count = 0;
+        int flag = 1;
+        //& 0xFFFF
+        while (flag != 0) {
+            if ((n & flag) > 0) {
+                count++;
+            }
+            flag = flag << 1;
+        }
+        return count;
+    }
+
+    private static int numberOfOneMinus(long n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = (n - 1) & n;
+        }
+        return count;
+    }
+
     private static void test6() {
         System.out.print("测试数0x80000000，期待值为1\t");
         System.out.print("左移法: " + numberOfOneLeft(0x80000000L));
@@ -44,38 +78,5 @@ public class NumberOf1InBinary {
         System.out.print("测试数0，期待值为0\t");
         System.out.print("左移法: " + numberOfOneLeft(0L));
         System.out.println("\t减一法: " + numberOfOneMinus(0L));
-    }
-
-    private static int numberOfOneRight(long n) {
-        int count = 0;
-        while (n != 0) {
-            if ((n & 1) > 0) {
-                count++;
-            }
-            n = n >> 1;
-        }
-        return count;
-    }
-
-    private static int numberOfOneLeft(long n) {
-        int count = 0;
-        int flag = 1;
-        //& 0xFFFF
-        while (flag != 0) {
-            if ((n & flag) > 0) {
-                count++;
-            }
-            flag = flag << 1;
-        }
-        return count;
-    }
-
-    private static int numberOfOneMinus(long n) {
-        int count = 0;
-        while (n != 0) {
-            count++;
-            n = (n - 1) & n;
-        }
-        return count;
     }
 }
