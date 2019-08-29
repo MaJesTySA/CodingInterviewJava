bc2b516b3326e587dec8445ca097a85c0f11eb51
MaJesTySA <363644935@qq.com>
2019-08-29 11:11:32 +0800

    refactoring

--- src/q24_反转链表/ReverseList.java
+++ src/q24_反转链表/ReverseList.java
@@ -4,17 +4,14 @@ import utils.ListNode;
 
 public class ReverseList {
     public static void main(String[] args) {
-        ListNode head = new ListNode(Integer.MIN_VALUE);
-        test1(head);
-        System.out.println("=================");
-        test2(head);
-        System.out.println("=================");
-        test3(head);
+        test1();
+        test2();
+        test3();
     }
 
     private static ListNode reverse(ListNode head) {
         ListNode reverseHead = null;
-        ListNode curNode = head.next;
+        ListNode curNode = head;
         ListNode preNode = null;
         while (curNode != null) {
             ListNode nextNode = curNode.next;
@@ -27,56 +24,37 @@ public class ReverseList {
         return reverseHead;
     }
 
-    private static void print(ListNode head) {
-        if (head == null || head.next == null) {
-            System.out.println("空链表");
-            return;
-        }
-        ListNode curListNode = head.next;
-        while (curListNode != null) {
-            System.out.print(curListNode.value + "->");
-            curListNode = curListNode.next;
-        }
-        System.out.println("NULL");
-    }
-
-    private static void test3(ListNode head) {
+    private static void test3() {
         System.out.println("空链表");
-        head.next = null;
-        System.out.print("反转前：");
-        print(head);
-        head.next = reverse(head);
-        System.out.print("反转后：");
-        print(head);
+        ListNode node1 = null;
+        print(node1);
     }
 
-    private static void test2(ListNode head) {
+    private static void test2() {
         System.out.println("输入链表只有一个节点");
-        head.next = new ListNode(1);
-        System.out.print("反转前：");
-        print(head);
-        head.next = reverse(head);
-        System.out.print("反转后：");
-        print(head);
+        ListNode node1 = new ListNode(1);
+        print(node1);
     }
 
-    private static void test1(ListNode head) {
+    private static void test1() {
         System.out.println("输入链表有多个节点：");
         ListNode node1 = new ListNode(1);
         ListNode node2 = new ListNode(2);
         ListNode node3 = new ListNode(3);
         ListNode node4 = new ListNode(4);
         ListNode node5 = new ListNode(5);
-        head.next = node1;
         node1.next = node2;
         node2.next = node3;
         node3.next = node4;
         node4.next = node5;
-        System.out.print("反转前：");
-        print(head);
-        head.next = reverse(head);
-        System.out.print("反转后：");
-        print(head);
+        print(node1);
     }
 
+    private static void print(ListNode node1) {
+        System.out.print("反转前：");
+        ListNode.print(node1);
+        System.out.print("反转后：");
+        ListNode.print(reverse(node1));
+        System.out.println("=============");
+    }
 }
